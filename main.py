9b6f3b05c21415637cb9576d57cec88a07be9bf3
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2020-12-04 21:48:53 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+basic.show_string("My Name is ")
+basic.show_string("Viva")
+basic.show_string("I love Harry Potter and Shawn Mendes")
+basic.show_string("I am ShawnPotter")
---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.showString("My Name is ")
+basic.showString("Viva")
+basic.showString("I love Harry Potter and Shawn Mendes")
+basic.showString("I am ShawnPotter")
